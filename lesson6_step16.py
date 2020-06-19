9451766609091cf92f987f84562ae7e52473d94f
lozbenev <lozbenev@lozbenev-pc.tgn.abcp.ru>
2020-06-19 10:41:02 +0300

    Занятие 16

---
+++ lesson6_step16.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+import time
+import math
+import os
+
+def calc(x):
+      return str(math.log(abs(12*math.sin(int(x)))))
+
+
+try:
+    browser = webdriver.Chrome()
+    browser.get("http://suninjuly.github.io/redirect_accept.html")
+    
+    button = browser.find_element_by_css_selector("button.btn").click()
+
+    new_window = browser.window_handles[1]
+    browser.switch_to.window(new_window)
+    
+         
+    x_element = browser.find_element_by_css_selector('label [id="input_value"]')
+    x = x_element.text
+    y = calc(x)
+
+    answer = browser.find_element_by_class_name( 'form-control' )
+    answer.send_keys(y)
+    
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+    
+ 
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+
