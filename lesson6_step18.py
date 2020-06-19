bf7406c7f4d6b32d772635a3a0852db980ce7a35
lozbenev <lozbenev@lozbenev-pc.tgn.abcp.ru>
2020-06-19 10:43:12 +0300

    Занятие 18

---
+++ lesson6_step18.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import math
+import os
+
+
+
+def calc(x):
+      return str(math.log(abs(12*math.sin(int(x)))))
+
+
+try:
+    browser = webdriver.Chrome()
+    browser.get("http://suninjuly.github.io/explicit_wait2.html")
+    
+   
+    button = WebDriverWait(browser, 12).until(
+        EC.text_to_be_present_in_element((By.ID, "price"), "$100"))
+
+    button = browser.find_element(By.ID, "book").click()
+    
+  
+
+    x_element = browser.find_element(By.ID, 'input_value')
+    x = x_element.text
+    y = calc(x)
+    
+
+    answer = browser.find_element(By.CLASS_NAME, 'form-control' ).send_keys(y)
+       
+    button1 = browser.find_element(By.ID, "solve").click()
+     
+    
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+
